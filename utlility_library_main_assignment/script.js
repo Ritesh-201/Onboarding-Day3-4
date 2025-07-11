324e277000c5b8d060a625ec562dc59e32d2e193
Ritesh Sharma <riteshsharma@Riteshs-MacBook-Air.local>
2025-07-11 11:59:15 +0530

    main-assignment

--- utlility_library_main_assignment/script.js
+++ utlility_library_main_assignment/script.js
@@ -5,16 +5,15 @@ function displayOutput(message) {
 
 // Utility Library
 const utils = {
-  // 1. Debounce: Delay function execution until 'delay' ms after last call
+
  debounce: (func, delay) => {
   let timeoutId;
   return (...args) => {
-    clearTimeout(timeoutId); // Cancel previous timeout
-    timeoutId = setTimeout(() => func(...args), delay); // Set new timeout
+    clearTimeout(timeoutId); 
+    timeoutId = setTimeout(() => func(...args), delay);
   };
 },
 
-  // 2. Throttle: Limit function to run once every 'limit' ms
  throttle: (func, limit) => {
   let lastCall = 0;
   return (...args) => {
@@ -26,16 +25,15 @@ const utils = {
   };
 },
 
-  // 3. Deep Merge: Recursively merge multiple objects
  deepMerge: (...objects) => {
   const isObject = (obj) => obj && typeof obj === "object" && !Array.isArray(obj);
   return objects.reduce((acc, obj) => {
     if (!isObject(obj)) return acc;
     Object.keys(obj).forEach((key) => {
       if (isObject(acc[key]) && isObject(obj[key])) {
-        acc[key] = utils.deepMerge(acc[key], obj[key]); // Recurse for nested objects
+        acc[key] = utils.deepMerge(acc[key], obj[key]); 
       } else {
-        acc[key] = obj[key]; // Overwrite or set value
+        acc[key] = obj[key]; 
       }
     });
     return acc;
@@ -48,5 +46,3 @@ isString: (value) => typeof value === "string",
 isNumber: (value) => typeof value === "number" && !isNaN(value),
 isFunction: (value) => typeof value === "function",
 };
-
-// Add test code here (see Testing section below)
